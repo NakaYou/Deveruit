273ebfc3c8fffbde29985a57c4be5f816fd2bcad
tkawakita <tommycsth61@gmail.com>
2021-04-25 06:03:07 +0900

    cardの大きさを調整

--- frontend/src/components/MyRecruitCard.tsx
+++ frontend/src/components/MyRecruitCard.tsx
@@ -9,12 +9,12 @@ const MyRecruitCard: FC<Props> = ({ title, imgPath }) => {
   // TODO: 削除時の処理を記述
   const deleteHandler = () => {};
   return (
-    <div className="mx-auto grid grid-cols-2 grid-rows-1 w-3/4 bg-gray-300 rounded-lg hover:opacity-90 hover:shadow-lg">
-      <img className="m-auto p-2" src={imgPath} alt="サムネイル画像" />
-      <div className="mr-3 grid grid-cols-1 grid-rows-3">
-        <div className="m-auto row-span-2 font-bold text-lg text-left">{title}</div>
+    <div className="mx-auto h-32 grid grid-cols-4 grid-rows-1 w-1/3 bg-gray-300 rounded-lg hover:opacity-90 hover:shadow-lg">
+      <img className="mx-auto my-auto h-3/4" src={imgPath} alt="サムネイル画像" />
+      <div className="mr-3 grid grid-cols-1 col-span-3 grid-rows-3">
+        <div className="m-auto row-span-2 text-xl text-left">{title}</div>
         <button
-          className="my-auto ml-auto w-20	h-8 rounded-lg bg-red-600"
+          className="my-auto ml-auto w-16 h-8 rounded-full bg-red-600"
           onClick={deleteHandler}
         >
           <span>削除</span>
