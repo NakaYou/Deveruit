54efc635b2fd8521b784f8fac46403bb34e66f98
tkawakita <tommycsth61@gmail.com>
2021-04-24 15:57:18 +0900

    確認用に設定したtitleを削除

--- frontend/src/components/MyRecruitCard.tsx
+++ frontend/src/components/MyRecruitCard.tsx
@@ -8,7 +8,6 @@ type Props = {
 const MyRecruitCard: FC<Props> = ({ title, imgPath }) => {
   // TODO: 削除時の処理を記述
   const deleteHandler = () => {};
-  title = "サポーターズのハッカソン！フロントエンド 募集！バックエンド募集！インフラも欲しい！"
   return (
     <div className="mx-auto grid grid-cols-2 grid-rows-1 w-3/4 bg-gray-300 rounded-lg hover:opacity-90 hover:shadow-lg">
       <img className="m-auto p-2" src={imgPath} alt="サムネイル画像" />
