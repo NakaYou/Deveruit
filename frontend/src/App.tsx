1dd873d7010ae7478b69e1b7daea92224595ea79
なかよう <72610232+NakaYou@users.noreply.github.com>
2021-04-25 03:16:03 +0900

    募集の詳細画面の作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import TimeLine from "./pages/Timeline";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import NewRecruitForm from "./pages/New";
-import Detail from "./pages/Details";
+import Details from "./pages/Details";
 import Notice from "./pages/Notice";
 import Recruits from "./pages/Recruits";
 import { Route, Routes } from "react-router-dom";
@@ -19,7 +19,7 @@ function App() {
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/new" element={<NewRecruitForm />} />
-      <Route path="/recruits/details/:id" element={<Detail />} />
+      <Route path="/recruits/details/:id" element={<Details />} />
       <Route path="/:username/notice" element={<Notice />} />
       <Route path="/:username/recruits" element={<Recruits />} />
     </Routes>
