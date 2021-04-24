619de443aaa3b24b5e3b18515b243b4dff511bda
tkawakita <tommycsth61@gmail.com>
2021-04-24 16:00:21 +0900

    titleの位置を修正

--- frontend/src/components/MyRecruitCard.tsx
+++ frontend/src/components/MyRecruitCard.tsx
@@ -12,7 +12,7 @@ const MyRecruitCard: FC<Props> = ({ title, imgPath }) => {
     <div className="mx-auto grid grid-cols-2 grid-rows-1 w-3/4 bg-gray-300 rounded-lg hover:opacity-90 hover:shadow-lg">
       <img className="m-auto p-2" src={imgPath} alt="サムネイル画像" />
       <div className="mr-3 grid grid-cols-1 grid-rows-3">
-        <div className="my-auto row-span-2 font-bold text-lg text-left">{title}</div>
+        <div className="m-auto row-span-2 font-bold text-lg text-left">{title}</div>
         <button
           className="my-auto ml-auto w-20	h-8 rounded-lg bg-red-600"
           onClick={deleteHandler}
