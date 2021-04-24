652701259c29ada6a2348916fc1a20edd2114eb5
tkawakita <tommycsth61@gmail.com>
2021-04-24 15:56:33 +0900

    自分がした募集のカードを作成

--- frontend/src/components/MyRecruitCard.tsx
+++ frontend/src/components/MyRecruitCard.tsx
@@ -1,1 +1,28 @@
-import React from "react";
+import React, { FC } from "react";
+
+type Props = {
+  title: string;
+  imgPath: string;
+};
+
+const MyRecruitCard: FC<Props> = ({ title, imgPath }) => {
+  // TODO: 削除時の処理を記述
+  const deleteHandler = () => {};
+  title = "サポーターズのハッカソン！フロントエンド 募集！バックエンド募集！インフラも欲しい！"
+  return (
+    <div className="mx-auto grid grid-cols-2 grid-rows-1 w-3/4 bg-gray-300 rounded-lg hover:opacity-90 hover:shadow-lg">
+      <img className="m-auto p-2" src={imgPath} alt="サムネイル画像" />
+      <div className="mr-3 grid grid-cols-1 grid-rows-3">
+        <div className="my-auto row-span-2 font-bold text-lg text-left">{title}</div>
+        <button
+          className="my-auto ml-auto w-20	h-8 rounded-lg bg-red-600"
+          onClick={deleteHandler}
+        >
+          <span>削除</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MyRecruitCard;
