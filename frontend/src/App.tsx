faa17d78f60d0b26c2778f5bc8d8a6521b1dfbac
なかよう <72610232+NakaYou@users.noreply.github.com>
2021-04-24 22:08:05 +0900

    ログインしてないときのヘッダーの作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom"
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header"
 
 function App() {
   return (
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,22 +3,44 @@ import { Link, Route } from "react-router-dom";
 import menu from "../images/menu.png"
 import close from "../images/close.png"
 
-type Props = {
+type MenuProps = {
     userName: string;
     iconPath: string;
 }
 
-type menuProps = {
+type HeaderProps = {
+    isLogin: boolean;
+    userName: string;
+    iconPath: string;
 }
 
-const Menubar: FC<Props> = ({ userName, iconPath }) => {
-    const [opningMenu, setmenu] = useState(false);
-    if (opningMenu) return (
+type SigninProps = {
+    
+}
+
+type BarProps ={
+    isLogin: boolean;
+    userName: string;
+    iconPath: string;
+}
+
+const Signinbar: FC<SigninProps> =() => {
+    return(
+        <div className="text-7x1 flex ml-auto text-white text-7xl ">
+            <Link to="/signup" className="mr-5">新規登録</Link>
+            <Link to="/signin">ログイン</Link>
+        </div>
+    )
+}
+
+const Menubar: FC<MenuProps> = ({ userName, iconPath }) => {
+    const [isOpenedMenu, setmenu] = useState(false);
+    if (isOpenedMenu) return (
         <div className="text-3xl text-right m-3 ml-auto mb-auto bg-green-300">
             <div className="flex ml-auto mb-auto">
                 <img className="rounded-full h-20" src={"https://avatars.githubusercontent.com/u/72610232?v=4"} alt="アイコン" />
                 <p className="  text-7xl ">{userName}</p>
-                <img className="w-20 h-20" src={close} onClick={() => setmenu(!opningMenu)} />
+                <img className="w-20 h-20" src={close} onClick={() => setmenu(!isOpenedMenu)} />
             </div>
             <div className="text-center leading-10 list-none">
                 <li>
@@ -44,17 +66,30 @@ const Menubar: FC<Props> = ({ userName, iconPath }) => {
         <div className="flex ml-auto mb-auto mt-auto ">
             <img className="rounded-full h-20 m-3 mr-0" src={"https://avatars.githubusercontent.com/u/72610232?v=4"} alt="アイコン" />
             <p className=" mt-3 mr-3 text-7xl mt-0 ">{userName}</p>
-            <img className="w-20 h-20" src={menu} onClick={() => setmenu(!opningMenu)} />
+            <img className="w-20 h-20" src={menu} onClick={() => setmenu(!isOpenedMenu)} />
         </div>
     )
 }
 
-const Header: FC<Props> = ({ userName, iconPath }) => {
+const Bar: FC<BarProps> = ({isLogin, userName, iconPath}) => {
+    if(isLogin){
+        return(
+            <Menubar userName={userName} iconPath={iconPath} />
+        )
+    }
+    else{
+        return(
+            <Signinbar />
+        )
+    }
+}
+
+const Header: FC<HeaderProps> = ({ isLogin, userName, iconPath }) => {
     return (
         <div className="flex">
             <div className="p-5 bg-green-300 flex mb-auto w-full">
                 <i className="text-7xl text-white">Deveruit</i>
-                <Menubar userName={userName} iconPath={iconPath} />
+                <Bar isLogin={isLogin} userName={userName} iconPath={iconPath}/>
             </div>
         </div>
     )
