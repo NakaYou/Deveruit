f92ad2b21cacfbb4b86ba110c17a28d8eb8a5e46
tkawakita <tommycsth61@gmail.com>
2021-04-24 14:43:10 +0900

    RecoilのインストールとRecoilRootでAppコンポーネントをラップ

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,15 @@
 import React from "react";
+import { RecoilRoot } from "recoil";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        やっていき
+      </div>
+    </RecoilRoot>
   );
 }
 
