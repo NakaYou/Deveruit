67612da8128e487da7eb3612c6d50b2cd094299a
なかよう <72610232+NakaYou@users.noreply.github.com>
2021-04-25 00:56:25 +0900

    承認と拒否の通知カード追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,14 @@ import { BrowserRouter } from "react-router-dom"
 import logo from "./logo.svg";
 import "./App.css";
 
+import NoticeCard from "./components/NoticeCard";
+
 function App() {
   return (
     <RecoilRoot>
       <BrowserRouter>
+        <NoticeCard cardtype="syounin" userName="NakaYou" iconPath="https://avatars.githubusercontent.com/u/72610232?v=4"
+        title="共同開発のタイトル" message="こんにちは" />
       </BrowserRouter>
     </RecoilRoot>
   );
